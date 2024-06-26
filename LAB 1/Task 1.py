8df9a536f30db9bc8d8601a5333e2d8b99424d33
Sayem- A ProgramoPhile <98210531+programophile@users.noreply.github.com>
2024-06-26 19:16:04 +0600

    complete

--- LAB 1/Task 1.py
+++ LAB 1/Task 1.py
@@ -2,17 +2,36 @@ import heapq
 graph={}
 heuristic_values={}
 def heuristic_update(all_info):
-    heuristic_values[all_info[0]]=all_info[1]
-    # print(heuristic_values)
-
+    heuristic_values[all_info[0]]=int(all_info[1])
 
 def create_graph(all_info):
     parent=all_info[0]
     graph[parent]={}
     for i in range (2,len(all_info),2):
-       child_node=all_info[i]
-       weight=int(all_info[i+1])
-       graph[parent][child_node]= weight
+           child_node=all_info[i]
+           weight=int(all_info[i+1])
+           graph[parent][child_node]= weight
+def AstarAlgo(start,goal):
+    heap_q=[]
+    heapq.heappush(heap_q,(int(heuristic_values[start]),0,start,[start]))
+
+    visited=[]
+    while heap_q:
+        _,weight,city,path=heapq.heappop(heap_q)
+        if city in visited:
+            continue
+        visited.append(city)
+        if city==goal:
+
+
+            return weight,path
+        for child,cost in graph[city].items():
+            total_weight=weight+cost
+            p_value= total_weight+heuristic_values[child]
+            heapq.heappush(heap_q, (p_value, total_weight, child,path+[child]))
+    return None
+
+
 
 with open('input_file.txt') as file:
     n = file.readlines()
@@ -22,4 +41,15 @@ with open('input_file.txt') as file:
         # print(all_info)
         heuristic_update(all_info)
         create_graph(all_info)
+
     print(graph)
+start=input("Enter start :")
+end=input("Enter End :")
+ans=AstarAlgo(start,end)
+output=open('output.txt',"w")
+if ans:
+    print(f"Path: {' -> '.join(ans[1])}")
+    print(f"Total distance: {ans[0]} km")
+    output.write(f"Path: {' -> '.join(ans[1])}\nTotal distance: {ans[0]} km")
+else:
+    print("No Path found")
